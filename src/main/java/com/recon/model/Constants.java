befc7ce58c2e25f5cccb4f62726f5d7028a5106c
vishu-chaudhary <vishal2421@outlook.com>
2018-07-17 10:22:26 +0530

    reduced jwt token time to 30 minutes

--- src/main/java/com/recon/model/Constants.java
+++ src/main/java/com/recon/model/Constants.java
@@ -2,7 +2,7 @@ package com.recon.model;
 
 public class Constants {
 
-	public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5 * 60 * 60;
+	public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 1800;
 	public static final String SIGNING_KEY = "sApIEnTTeChNoLoGiEs";
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
